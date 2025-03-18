6d178d48e70adcb4f5acad7c07fc19e22be08786
zaif <zaifbh4708@gmail.com>
2025-03-17 21:12:26 -0400

    changes

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -8,68 +8,68 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-public class Player extends Entity{
+public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 
-    public Player(GamePanel gp, KeyHandler keyH){
+    public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
         setDefaultValues();
         getPlayerImage();
     }
-    public void setDefaultValues() {
 
+    public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
         direction = "down";
     }
+
     public void getPlayerImage() {
         try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkup1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkup2.png"));
+            up3 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkup3.png"));
+            up4 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkup4.png"));
 
-            up1 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkup1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkup2.png"));
-            up3 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkup3.png"));
-            up4 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkup4.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkdown1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkdown2.png"));
+            down3 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkdown3.png"));
+            down4 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkdown4.png"));
 
-            down1 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkdown1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkdown2.png"));
-            down3 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkdown3.png"));
-            down4 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkedown4.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkleft1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkleft2.png"));
 
-            left1 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkuleft1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkleft2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkright1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/playerwalkright2.png"));
 
-            right1 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkright1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/player/playerwalkright2.png"));
-
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void update(){
-        if (keyH.upPressed == true){
+
+    public void update() {
+        if (keyH.upPressed) {
             direction = "up";
             y -= speed;
-        }
-        else if (keyH.downPressed == true){
+        } else if (keyH.downPressed) {
             direction = "down";
             y += speed;
         }
-        if (keyH.leftPressed == true){
+        if (keyH.leftPressed) {
             direction = "left";
             x -= speed;
-        }
-        else if (keyH.rightPressed == true){
+        } else if (keyH.rightPressed) {
             direction = "right";
             x += speed;
         }
     }
-    public void draw(Graphics2D g2){
+
+    public void draw(Graphics2D g2) {
         BufferedImage image = null;
 
-        switch(direction){
+        switch (direction) {
             case "up":
                 image = up1;
                 break;
@@ -83,11 +83,6 @@ public class Player extends Entity{
                 image = right1;
                 break;
         }
-        g2.drawImage(image, x, y, 48,48, null);
-
-
-
-
-
+        g2.drawImage(image, x, y, 48, 48, null);
     }
 }
